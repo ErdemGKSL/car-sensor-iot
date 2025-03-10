56a96acfc589f189ba0867c2b5df0592ec45f989
Erdem <zahrakon06@gmail.com>
2025-03-10 19:31:17 +0300

    Update vite.config.ts

--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -1,7 +1,10 @@
 import tailwindcss from "@tailwindcss/vite";
 import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import { defineConfig, preview } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit(), tailwindcss()]
+	plugins: [sveltekit(), tailwindcss()],
+	preview: {
+		allowedHosts: ['iot.erdemdev.tr']
+	}
 });
